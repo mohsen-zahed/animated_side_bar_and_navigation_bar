ec10b66deb7403c44610ec64a7d9fabef384604a
amir <mohsenzahed077@gmail.com>
2023-01-24 15:59:32 +0430

    navigation bar was completed

---
+++ android/app/src/main/kotlin/com/example/animated_side_bar_and_navigation_bar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_side_bar_and_navigation_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list.dart
@@ -0,0 +1,40 @@
+import 'package:animated_side_bar_and_navigation_bar/rive_asset.dart';
+
+List<RiveAsset> bottomNavs = [
+  RiveAsset(
+    "assets/images/1298-2487-animated-icon-set-1-color.riv",
+    artboard: "CHAT",
+    stateMachineName: "CHAT_Interactivity",
+    title: "Chat",
+  ),
+  RiveAsset(
+    "assets/images/1298-2487-animated-icon-set-1-color.riv",
+    artboard: "SEARCH",
+    stateMachineName: "SEARCH_Interactivity",
+    title: "Search",
+  ),
+  RiveAsset(
+    "assets/images/1298-2487-animated-icon-set-1-color.riv",
+    artboard: "TIMER",
+    stateMachineName: "TIMER_Interactivity",
+    title: "Timer",
+  ),
+  RiveAsset(
+    "assets/images/1298-2487-animated-icon-set-1-color.riv",
+    artboard: "USER",
+    stateMachineName: "USER_Interactivity",
+    title: "Profile",
+  ),
+  RiveAsset(
+    "assets/images/1298-2487-animated-icon-set-1-color.riv",
+    artboard: "USER",
+    stateMachineName: "USER_Interactivity",
+    title: "Profile",
+  ),
+  RiveAsset(
+    "assets/images/1298-2487-animated-icon-set-1-color.riv",
+    artboard: "USER",
+    stateMachineName: "USER_Interactivity",
+    title: "Profile",
+  ),
+];
