8f7f5cee162d7ed2289fec8bec6fa3ead72518d4
amir <mohsenzahed077@gmail.com>
2023-01-24 16:04:03 +0430

    navigation bar was completed

--- lib/components/bottom_vertical_widget.dart
+++ lib/components/bottom_vertical_widget.dart
@@ -2,8 +2,8 @@ import 'package:animated_side_bar_and_navigation_bar/list.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-import '../rive_asset.dart';
-import '../rive_utiles.dart';
+import 'rive_asset.dart';
+import 'rive_utiles.dart';
 
 class BottomVerticalWidget extends StatefulWidget {
   const BottomVerticalWidget({
